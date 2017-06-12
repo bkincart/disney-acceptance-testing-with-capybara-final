9beccca8d418927069366ed5635c62a1781f3fb1
Brianna <briannakincart@gmail.com>
2017-06-12 17:25:35 -0400

    first updated commit

---
+++ server.rb
@@ -0,0 +1,56 @@
+require "sinatra"
+require "pry"
+require "csv"
+
+
+set :bind, "0.0.0.0"
+set :views, File.join(File.dirname(__FILE__), "views")
+
+
+get "/" do
+  redirect to("/movies")
+end
+
+get "/movies" do
+  @movies = []
+  CSV.foreach(csv_file, headers: true) do |row|
+    @movies << row.to_h
+  end
+  erb :"movies/index"
+end
+
+get "/movies/new" do
+  erb :"movies/new"
+end
+
+post "/movies/new" do
+  title = params["title"]
+  release_year = params["release_year"]
+  runtime = params["runtime"]
+  if [title, release_year, runtime].include?("")
+    @error = true
+    erb :"movies/new"
+  else
+    CSV.open(csv_file, "a", headers: true) do |csv|
+      csv << [params["title"], params["release_year"], params["runtime"]]
+    end
+    redirect "/movies"
+  end
+end
+
+
+# Helper Methods
+
+def csv_file
+  if ENV["RACK_ENV"] == "test"
+    "data/movies_test.csv"
+  else
+    "data/movies.csv"
+  end
+end
+
+def reset_csv
+  CSV.open(csv_file, "w", headers: true) do |csv|
+    csv << ["title", "release_year", "runtime"]
+  end
+end
